8c8157c8bf3b7331f62492c333fb3992dc7bf924
zbie <ejoachimgabin@gaming.tech>
2022-03-08 18:45:15 +0100

    merge problème

--- app/src/components/Article.js
+++ app/src/components/Article.js
@@ -4,24 +4,18 @@ import { Button, Card } from 'react-bootstrap';
 
 const Article = (props) => {
 
-<<<<<<< HEAD
-=======
+
 	let article = props.article.attributes;
 
->>>>>>> b69240796607c099315732a1e794196e217158b9
 	return (
 		<Card border="light" style={{ width: '18rem', background: "#404040", minHeight: "500px", marginBottom: "2rem" }}>
 			<Card.Img variant="top" src={article.thumbnail.data == null ? "" : ("http://localhost:1337" + article.thumbnail.data.attributes.url)} />
 			<Card.Body className="d-flex flex-column">
 				<Card.Title>{article.name}</Card.Title>
 				<Card.Text>
-<<<<<<< HEAD
-					{props.article.price}€
-				</Card.Text>				
-=======
+				
 					{article.price}€
 				</Card.Text>
->>>>>>> b69240796607c099315732a1e794196e217158b9
 				<Button className="mt-auto" onClick={() => props.addArticleToCart(props.article)} variant="primary">Ajouter au panier</Button>
 			</Card.Body>
 		</Card>
