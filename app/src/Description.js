639c28ebc56c9cda17491df32f19dff1400df837
unknown <ejoachimgabin@gamingcampus.local>
2022-03-09 13:05:49 +0100

    Add the buttom in description

--- app/src/Description.js
+++ app/src/Description.js
@@ -8,8 +8,8 @@ import Footer from "./components/Footer";
 
 class Description extends React.Component {
     
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             article: [],
             id: -1,
@@ -25,7 +25,7 @@ class Description extends React.Component {
         let response = await fetch("http://localhost:1337/api/articles/" + id + "?populate=*", {method: "GET", headers: {"Accept": "application/json", "Content-Type": "application/json"}});
         const a = await response.json();
 
-        this.setState({ id: id, article: a.data.attributes, loading: false});
+        this.setState({ id: id, article: a.data, loading: false});
     }
 
     render() {
@@ -48,15 +48,17 @@ class Description extends React.Component {
 				
 				<Container border="light" className="main-content" style={{"padding": "60px", "background": "#404040", "marginTop": "100px", "marginBottom": "140px"}}>
 					<div class="underline">
-						<h1 class="text-center">{this.state.article.name}</h1>                
+						<h1 class="text-center">{this.state.article.attributes.name}</h1>                
 					</div>
 
 					<div className="text-center mb-5">
                         <div style={{"marginTop": "75px", "marginBottom": "75px"}}>
-						    {this.state.article.thumbnail.data == null ? <p class="anim-alarm red-light">/!\ IMAGE CONFIDENTIELLE /!\</p> : <img src={("http://localhost:1337" + this.state.article.thumbnail.data.attributes.url)}></img>}
+						    {this.state.article.attributes.thumbnail.data == null ? <p class="anim-alarm red-light">/!\ IMAGE CONFIDENTIELLE /!\</p> : <img src={("http://localhost:1337" + this.state.article.attributes.thumbnail.data.attributes.url)}></img>}
 						</div>
-                        <p>{this.state.article.description}</p>
-						<Link to="/articles">
+                        <p>{this.state.article.attributes.description}</p>
+
+                        <Button onClick={() => this.props.addArticleToCart(this.state.article)} variant="primary">Ajouter au panier</Button>						
+                        <Link to="/articles">
 							<Button size="lg">Retourner a la boutique</Button>
 						</Link>
 					</div>
--- app/src/components/Article.js
+++ app/src/components/Article.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import { Button, Card } from 'react-bootstrap';
+import { Button, Card, Container } from 'react-bootstrap';
+import info from '../img/article-info.png';
+import redacted from '../img/Redacted.webp';
 
 
 const Article = (props) => {
@@ -9,7 +11,8 @@ const Article = (props) => {
 
 	return (
 		<Card border="light" style={{ width: '18rem', background: "#404040", minHeight: "500px", marginBottom: "2rem" }}>
-			<Card.Img variant="top" src={article.thumbnail.data == null ? "" : ("http://localhost:1337" + article.thumbnail.data.attributes.url)} />
+			<Card.Img variant="top" src={article.thumbnail.data == null ? redacted : ("http://localhost:1337" + article.thumbnail.data.attributes.url)} />
+			
 			<Card.Body className="d-flex flex-column">
 				<Card.Title>{article.name}</Card.Title>
 				<Card.Text>
@@ -17,8 +20,13 @@ const Article = (props) => {
 					{article.price}€
 
 				</Card.Text>
-				<Link to={"/description?id=" + props.article.id}><Button>Description</Button></Link>				
-				<Button className="mt-auto" onClick={() => props.addArticleToCart(props.article)} variant="primary">Ajouter au panier</Button>
+				<div className="mt-auto d-flex">
+
+					<Button onClick={() => props.addArticleToCart(props.article)} style={{alignItems: "stretch", flex: "1"}} variant="primary">Ajouter au panier</Button>
+
+					<Link to={"/description?id=" + props.article.id}><Button className="mt-auto" style={{marginLeft: "8px"}}><img src={info} style={{width: "32px", height: "32px"}}></img></Button></Link>
+
+				</div>				
 			</Card.Body>
 		</Card>
 	);
