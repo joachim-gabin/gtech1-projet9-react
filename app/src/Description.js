c0cd799736f61515608ec79d90a55d64adc521cf
nwiart <nwiart@gaming.tech>
2022-03-09 09:31:17 +0100

    Fixed description.js conflict again

--- app/src/Description.js
+++ app/src/Description.js
@@ -5,7 +5,7 @@ import { Link, renderMatches } from 'react-router-dom';
 import MenuBar from "./components/MenuBar";
 import Footer from "./components/Footer";
 
-/* match.params.id*/
+
 class Description extends React.Component {
     
     constructor() {
@@ -22,13 +22,10 @@ class Description extends React.Component {
         const urlParams = await new URLSearchParams(queryString);
         const id = await urlParams.get("id");
 
-
-        let response = await fetch("http://localhost:1337/api/articles/"+id+ "?populate=*", {method: "GET", headers: {"Accept": "application/json", "Content-Type": "application/json"}});
+        let response = await fetch("http://localhost:1337/api/articles/" + id + "?populate=*", {method: "GET", headers: {"Accept": "application/json", "Content-Type": "application/json"}});
         const a = await response.json();
+
         this.setState({ id: id, article: a.data.attributes, loading: false});
-
-        
-
     }
 
     render() {
@@ -37,37 +34,36 @@ class Description extends React.Component {
         {
             return (
                 <>
-                <MenuBar articles={this.props.cart} />                
-                <p>Chargement en cours</p>
+                    <MenuBar articles={this.props.cart} />                
+                    <p>Chargement en cours</p>
                 </>
             );
         }
 
 
         return(
-        <>
-        <MenuBar articles={this.props.cart} />        
-        <p>{JSON.stringify(this.state.article)}</p>
-        
-        <Container className="main-content" style={{"padding": "60px", "background": "#404040", "marginTop": "100px", "marginBottom": "140px"}}>
-            <div>
-                <h1 class="text-center">{this.state.article.name}</h1>                
-            </div>
+			<>
+				<MenuBar articles={this.props.cart} />   
 
-            <div className="text-center mb-5">
+				<p>{JSON.stringify(this.state.article)}</p>
+				
+				<Container className="main-content" style={{"padding": "60px", "background": "#404040", "marginTop": "100px", "marginBottom": "140px"}}>
+					<div>
+						<h1 class="text-center">{this.state.article.name}</h1>                
+					</div>
 
-                {this.state.article.thumbnail.data == null ? <p class="red-light">/!\ IMAGE CONFIDENTIELLE /!\</p> : <img src={("http://localhost:1337" + this.state.article.thumbnail.data.attributes.url)}></img>}
-                <p>{this.state.article.description}</p>
-                <Link to="/articles">
-                    <Button size="lg">Retourner a la boutique</Button>
-                </Link>
-            </div>
-        </Container>
-        <Footer />
-        </>
+					<div className="text-center mb-5">
 
-
-
+						{this.state.article.thumbnail.data == null ? <p class="red-light">/!\ IMAGE CONFIDENTIELLE /!\</p> : <img src={("http://localhost:1337" + this.state.article.thumbnail.data.attributes.url)}></img>}
+						<p>{this.state.article.description}</p>
+						<Link to="/articles">
+							<Button size="lg">Retourner a la boutique</Button>
+						</Link>
+					</div>
+				</Container>
+				
+				<Footer />
+			</>
         );
     }
 }
